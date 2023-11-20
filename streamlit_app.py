4977d7dad4061e13458f403a4a3472712d8340fa
snowflakemargaret <151553359+snowflakemargaret@users.noreply.github.com>
2023-11-20 11:13:10 -0700

    Update streamlit_app.py
    Fixed issue with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import streamlit
 streamlit.title('My Parents Now Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞'Avocado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
