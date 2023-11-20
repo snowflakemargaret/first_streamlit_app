b391cda13d28b611e62cb617c1a366b0a6a25802
snowflakemargaret <151553359+snowflakemargaret@users.noreply.github.com>
2023-11-20 11:07:39 -0700

    Update streamlit_app.py
    Trying commit again

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title('My Parents Now Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header ('Breakfast Menu')
+streamlit.text ('Omega 3 Blueberry Oatmeal')
+streamlit.text ('Kale, Spinach & Rocket Smoothie')
+streamlit.text ('Hard-Boiled Free-Range Egg')
